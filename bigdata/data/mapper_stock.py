2ba49d3e880d92cc2f87d5320d6d119f5277cc21
phuongnguyen2106 <phuong.nguyen@wizeline.com>
2021-08-22 09:55:59 +0700

    data

---
+++ bigdata/data/mapper_stock.py
@@ -0,0 +1,6 @@
+import sys
+
+for line in sys.stdin:
+    part = line.split(',')    
+    print (part[0], 1)
+    
---
+++ bigdata/data/reducer_stock.py
@@ -0,0 +1,13 @@
+import sys
+from operator import itemgetter 
+wordcount = {}
+
+for line in sys.stdin:
+    word,count = line.split(' ')
+    count = int(count)
+    wordcount[word] = wordcount.get(word,0) + count
+
+sorted_wordcount = sorted(wordcount.items(), key=itemgetter(0))
+
+for word, count in sorted_wordcount:
+    print ('%s\t%s'% (word,count))
