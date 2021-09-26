831e243468455c6fb04ae36b5c92e0ca3b593b90
phuongnguyen2106 <phuong.nguyen@wizeline.com>
2021-09-26 22:21:20 +0700

    lab10

--- 12-SequenceModel/coursera/lab10/utils.py
+++ 12-SequenceModel/coursera/lab10/utils.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def mergeIntervals(intervals):
     sorted_by_lower_bound = sorted(intervals, key=lambda tup: tup[0])
     merged = []
@@ -23,14 +22,13 @@ def mergeIntervals(intervals):
                 merged.append(higher)
     return merged
 
-
 def get_entities(df):
-
+    
     entities = []
-
+    
     for i in range(len(df)):
         entity = []
-
+    
         for annot in df['annotation'][i]:
             try:
                 ent = annot['label'][0]
@@ -39,26 +37,24 @@ def get_entities(df):
                 entity.append((start, end, ent))
             except:
                 pass
-
+    
         entity = mergeIntervals(entity)
         entities.append(entity)
-
+    
     return entities
 
-
-def read_dataset():
+def read_dataset()
     df_data = pd.read_json("ner.json", lines=True)
     df_data = df_data.drop(['extras'], axis=1)
     df_data['content'] = df_data['content'].str.replace("\n", " ")
     df_data['entities'] = get_entities(df_data)
-
+    
     return df_data
-
 
 def convert_dataturks_to_spacy(dataturks_JSON_FilePath):
     try:
         training_data = []
-        lines = []
+        lines=[]
         with open(dataturks_JSON_FilePath, 'r') as f:
             lines = f.readlines()
 
@@ -69,7 +65,7 @@ def convert_dataturks_to_spacy(dataturks_JSON_FilePath):
             data_annotations = data['annotation']
             if data_annotations is not None:
                 for annotation in data_annotations:
-                    # only a single point in text annotation.
+                    #only a single point in text annotation.
                     point = annotation['points'][0]
                     labels = annotation['label']
                     # handle both list of labels or a single label.
@@ -80,23 +76,19 @@ def convert_dataturks_to_spacy(dataturks_JSON_FilePath):
                         point_start = point['start']
                         point_end = point['end']
                         point_text = point['text']
-
-                        lstrip_diff = len(point_text) - \
-                            len(point_text.lstrip())
-                        rstrip_diff = len(point_text) - \
-                            len(point_text.rstrip())
+                        
+                        lstrip_diff = len(point_text) - len(point_text.lstrip())
+                        rstrip_diff = len(point_text) - len(point_text.rstrip())
                         if lstrip_diff != 0:
                             point_start = point_start + lstrip_diff
                         if rstrip_diff != 0:
                             point_end = point_end - rstrip_diff
-                        entities.append((point_start, point_end + 1, label))
-            training_data.append((text, {"entities": entities}))
+                        entities.append((point_start, point_end + 1 , label))
+            training_data.append((text, {"entities" : entities}))
         return training_data
     except Exception as e:
-        logging.exception("Unable to process " +
-                          dataturks_JSON_FilePath + "\n" + "error = " + str(e))
+        logging.exception("Unable to process " + dataturks_JSON_FilePath + "\n" + "error = " + str(e))
         return None
-
 
 def trim_entity_spans(data: list) -> list:
     """Removes leading and trailing white spaces from entity spans.
@@ -124,8 +116,7 @@ def trim_entity_spans(data: list) -> list:
                 valid_end -= 1
             valid_entities.append([valid_start, valid_end, label])
         cleaned_data.append([text, {'entities': valid_entities}])
-    return cleaned_data
-
+    return cleaned_data  
 
 def clean_dataset(data):
     cleanedDF = pd.DataFrame(columns=["setences_cleaned"])
@@ -139,24 +130,23 @@ def clean_dataset(data):
         strDictData = data[i][1]
         lastIndexOfSpace = strData.rfind(' ')
         for i in range(lenOfString):
-            if (strData[i] == " " and strData[i+1] != " "):
-                for k, v in strDictData.items():
+            if (strData[i]==" " and strData[i+1]!=" "):
+                for k,v in strDictData.items():
                     for j in range(len(v)):
                         entList = v[len(v)-j-1]
-                        if (start >= int(entList[0]) and i <= int(entList[1])):
+                        if (start>=int(entList[0]) and i<=int(entList[1])):
                             emptyList[numberOfWords] = entList[2]
                             break
                         else:
                             continue
-                start = i + 1
+                start = i + 1  
                 numberOfWords += 1
             if (i == lastIndexOfSpace):
                 for j in range(len(v)):
-                    entList = v[len(v)-j-1]
-                    if (lastIndexOfSpace >= int(entList[0]) and lenOfString <= int(entList[1])):
-                        emptyList[numberOfWords] = entList[2]
-                        numberOfWords += 1
-        cleanedDF = cleanedDF.append(
-            pd.Series([emptyList],  index=cleanedDF.columns), ignore_index=True)
+                        entList = v[len(v)-j-1]
+                        if (lastIndexOfSpace>=int(entList[0]) and lenOfString<=int(entList[1])):
+                            emptyList[numberOfWords] = entList[2]
+                            numberOfWords += 1
+        cleanedDF = cleanedDF.append(pd.Series([emptyList],  index=cleanedDF.columns ), ignore_index=True )
         sum1 = sum1 + numberOfWords
     return cleanedDF
